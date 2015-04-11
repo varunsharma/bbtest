df71c3e08c31b048dfe0687ab430c48daa71d134
Varun Sharma <varun@sharmalabs.org>
2015-04-12 02:34:22 +0530

    Add test

--- tests/test_add.py
+++ tests/test_add.py
@@ -7,6 +7,9 @@ class TestAdd(unittest.TestCase):
     def test_add_fails(self):
         self.assertEqual(add(1,2), 5)
 
+    def test_add_passes(self):
+        self.assertEqual(add(1,2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
