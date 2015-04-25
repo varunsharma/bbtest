71e862fba8ffe2df2c4487294e57e6a801bc46f2
Varun Sharma <varun@sharmalabs.org>
2015-04-25 23:53:46 +0530

    Add failing test

--- tests/test_add.py
+++ tests/test_add.py
@@ -4,8 +4,8 @@ from bbtest.add import add
 
 class TestAdd(unittest.TestCase):
 
-#    def test_add_fails(self):
-#        self.assertEqual(add(1,2), 5)
+    def test_add_fails(self):
+        self.assertEqual(add(1,2), 5)
 
     def test_add_passes(self):
         self.assertEqual(add(1,2), 3)
