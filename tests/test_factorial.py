495d3a5ed18c41a9c4044ef28fb1a8c0ffb3b15c
Varun Sharma <varun@sharmalabs.org>
2015-05-01 23:51:30 +0530

    Update test_factorial with failing case

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -5,9 +5,13 @@ from bbtest.fact import factorial
 
 class TestFactorial(unittest.TestCase):
 
-    def test_factorial(self):
+    def test_factorial_passes(self):
         self.assertEqual(factorial(5), 120)
         self.assertNotEqual(factorial(2), 120)
+
+    def test_factorial_fails(self):
+        self.assertEqual(factorial(5), 121)
+        self.assertEqual(factorial(2), 121)
 
 
 if __name__ == '__main__':
